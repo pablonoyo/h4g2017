c30d6e89cdba31fdfe7c10b6563433e74095e8bb
jesus-cruz <cruzoliverajesus@gmail.com>
2017-03-10 19:51:17 +0100

    ino comments

--- arduino/juegoRepetirLeds/juegoRepetirLeds.ino
+++ arduino/juegoRepetirLeds/juegoRepetirLeds.ino
@@ -1,8 +1,12 @@
+// Los pines donde están conectados los leds
 int posLeds[4]={8,9,10,11};
 
+// Los pines donde están conectados los botones
 int posButtons[4] ={2,3,4,5};
 
+// El orden en el que se encenderán los pines
 int cicloLeds[4] = {8,9,10,11};
+
 
 void setup() {
     pinMode(posLeds[0], OUTPUT);
@@ -17,7 +21,6 @@ void setup() {
   
 }
 
-// the loop function runs over and over again forever
 void loop() {
     calcularCicloRandom();
     ejecutarCicloLed();
@@ -25,13 +28,14 @@ void loop() {
     delay(1000);
 }
 
-
+// Calculamos el contenido de un array de cuatro posiciones con valores entre 8 y 11 ( los pines )
 void calcularCicloRandom(){
     for ( int i = 0; i < 4; i++ ) {
         cicloLeds[i] = random(8,12);
     }
 }
 
+// Ejecutamos el ciclo calculado anteriormente
 void ejecutarCicloLed(){
     for ( int i = 0; i < 4; i++ ){
         delay(500);
@@ -41,6 +45,7 @@ void ejecutarCicloLed(){
     }
 }
 
+// Ha habido un fallo, encendemos el led rojo
 void indicarFallo(){
     digitalWrite(9,HIGH);
     delay(500);
@@ -48,13 +53,13 @@ void indicarFallo(){
     delay(500);
 }
 
-
-
+// Vamos comprobando la entrada del usuario, no saldremos hasta que sea la correcta
 void comprobarEntrada(){
     for ( int i = 0; i < 4; i++) {
-        while ( digitalRead(posButtons[ cicloLeds[i] - 8 ]) == LOW){
+        while ( digitalRead(posButtons[ cicloLeds[i] - 8 ]) == LOW){    // Si no es el correcto...
             delay(10);
         }
+        // Notificamos que el led encendido es el correcto
         digitalWrite( cicloLeds[i], HIGH);
         delay(200);
         digitalWrite( cicloLeds[i], LOW);
@@ -63,6 +68,7 @@ void comprobarEntrada(){
     ejecutarCicloExito();
 }
 
+// Una animación con leds para indicar el éxito
 void ejecutarCicloExito(){
     for ( int i = 0; i < 8; i++ ){
         delay(50);
